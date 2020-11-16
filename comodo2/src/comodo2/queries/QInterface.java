19174043b1647564091cd5d16318521f64a05b35
Luigi Andolfato <landolfa@eso.org>
2020-11-16 18:08:24 +0100

    Started to add support for SysML InterfaceBlock ELT MAL/RAD platform.

--- comodo2/src/comodo2/queries/QInterface.java
+++ comodo2/src/comodo2/queries/QInterface.java
@@ -4,6 +4,7 @@ import comodo2.engine.Config;
 import javax.inject.Inject;
 import org.eclipse.uml2.uml.Element;
 import org.eclipse.uml2.uml.Interface;
+import org.eclipse.uml2.uml.Class;
 import org.eclipse.uml2.uml.Reception;
 
 public class QInterface {
@@ -17,11 +18,37 @@ public class QInterface {
 		return i.getNearestPackage();
 	}
 
+	public org.eclipse.uml2.uml.Package getContainerPackage(final Class c) {
+		return c.getNearestPackage();
+	}
+
+	/**
+	 * @param i UML Interface .
+	 * @return true if i has <<cmdoInterface>> stereotype, false otherwise.
+	 */
 	public boolean isToBeGenerated(final Interface i) {
 		if (mQStereotype.isComodoInterface(((Element) i)) == false) {
 			return false;
 		}
 		return mQPackage.isParentComodoModule(i.getNearestPackage(), Config.getInstance().getCurrentModule());
+	}
+
+	/**
+	 * @param c UML Class (e.g. SysML BlockInterface)
+	 * @return true if c has <<cmdoInterface>> stereotype, false otherwise
+	 */
+	public boolean isToBeGenerated(final Class c) {
+		if (mQStereotype.isComodoInterface(((Element) c)) == false) {
+			return false;
+		}
+		return mQPackage.isParentComodoModule(c.getNearestPackage(), Config.getInstance().getCurrentModule());
+	}
+
+	public boolean isToBeGenerated(final Element e) {
+		if (mQStereotype.isComodoInterface(e) == false) {
+			return false;
+		} 
+		return mQPackage.isParentComodoModule(e.getNearestPackage(), Config.getInstance().getCurrentModule());
 	}
 
 	public boolean hasRequests(final Interface i) {
@@ -32,4 +59,14 @@ public class QInterface {
 		}
 		return false;
 	}
+
+	public boolean hasRequests(final Class c) {
+		for (final Reception r : c.getOwnedReceptions()) {
+			if (mQStereotype.isComodoCommand(((Element) r.getSignal()))) {
+				return true;
+			}
+		}
+		return false;
+	}
+
 }
