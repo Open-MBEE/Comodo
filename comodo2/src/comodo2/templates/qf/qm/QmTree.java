e635d786f4e2ba75c863e67f366a1136c4d24c62
chammard <thomas.boyer.chammard@jpl.nasa.gov>
2021-06-14 16:20:19 -0700

    Added getNodeByName and getRelativePath to QmTree

--- comodo2/src/comodo2/templates/qf/qm/QmTree.java
+++ comodo2/src/comodo2/templates/qf/qm/QmTree.java
@@ -1,20 +1,22 @@
 package comodo2.templates.qf.qm;
 
 import java.util.ArrayList;
+import java.util.Hashtable;
 import java.util.Objects;
 
-
 /**
- * This class will be used to represent a model or state machine so that we can retrieve relative paths between elements
- * This is because QM expects a relative path to be passed as a target.
+ * This class will be used to represent a model or state machine so that we can
+ * retrieve relative paths between elements This is because QM expects a
+ * relative path to be passed as a target.
  */
 
 public class QmTree {
-    
+
     String nodeName;
-    QmTree parent;
+    QmTree parent; // not used for now, could maybe remove it.
     ArrayList<QmTree> children;
     Integer depth;
+    // QmTree rootNode;
 
     public QmTree(String nodeName) {
         this.nodeName = nodeName;
@@ -28,18 +30,52 @@ public class QmTree {
 
     public QmTree addChild(String child) {
         QmTree childNode = new QmTree(child);
+
         childNode.parent = this;
-        this.children.add(childNode);
         childNode.depth = this.depth + 1;
+
+        this.children.add(childNode);
+
         return childNode;
     }
 
     /**
-     * Return the relative path between source and target by going up to the calling element (this) and exploring the subtrees
-     * Most likely, this will only be called from the root of the state machine
+     * Recursively checks within all of this instance's children for a node that has the name nodeName
+     * @param nodeName name to match
+     * @return Matched node, null if no match
+     */
+    public QmTree getNodeByName(String nodeName) {
+
+        if (this.children.isEmpty()) {
+            return null; // case where we have explored this entire subtree but not found
+        }
+
+        QmTree found_node = this.children.stream().filter(qmTree -> nodeName.equals(qmTree.nodeName)).findFirst().orElse(null);
+
+        if (!Objects.equals(null, found_node)) {
+            return found_node;
+        } else {
+            for (final QmTree child : this.children) {
+
+                QmTree rec_found_node = child.getNodeByName(nodeName);
+
+                if (!Objects.equals(rec_found_node, null)) {
+                    return rec_found_node;
+                }
+            }
+
+        }
+        return null;
+    }
+
+    /**
+     * Return the relative path between source and target by going up to the calling
+     * element (this) and exploring the subtrees Most likely, this will only be
+     * called from the root of the state machine
+     * 
      * @return Relative path between source and target, as a String.
      */
-    public String getRelativePath(QmTree source, QmTree target){
+    public String getRelativePath(QmTree source, QmTree target) {
         String path_to_root = stringRepeat(source.depth, "../");
 
         return path_to_root + this.findDownwardsPath(target);
@@ -47,38 +83,43 @@ public class QmTree {
 
     /**
      * Recursively searches through this isntance's children for an element.
-     * @param el to search for
-     * @return String path
+     * 
+     * @param el target to search for
+     * @return String path from this instance towards target
      */
     public String findDownwardsPath(QmTree el) {
 
         if (this.children.isEmpty()) {
             return ""; // case where we have explored this entire subtree but not found
         }
+
         if (this.children.contains(el)) {
             return Integer.toString(this.children.indexOf(el));
         } else {
+
             for (final QmTree child : this.children) {
+
                 String down_path = child.findDownwardsPath(el);
+
                 if (!Objects.equals("", down_path)) {
-                    System.out.println(this.children.indexOf(child));
                     return Integer.toString(this.children.indexOf(child)) + "/" + child.findDownwardsPath(el);
                 }
             }
             return "";
         }
     }
-    
+
     /**
      * Used to get a relative path up to root element
+     * 
      * @param count Number of times to repeat
-     * @param with String to be repeated
+     * @param with  String to be repeated
      * @return
      */
     public static String stringRepeat(int count, String with) {
         return new String(new char[count]).replace("\0", with);
     }
-    
+
     public static void main(String[] args) {
 
         QmTree stateMachine = new QmTree("SM");
@@ -89,16 +130,15 @@ public class QmTree {
 
         QmTree off_timeout = off.addChild("off_timeout");
         QmTree on_timeout = on.addChild("on_timeout");
-        
 
-        System.out.println(off.depth); 
-        System.out.println(stateMachine.children); 
+        QmTree off_eetimeout = off_timeout.addChild("offee_timeout");
 
-        System.out.println("Downwards: " + stateMachine.findDownwardsPath(on_timeout));
-        System.out.println("Relative: " + stateMachine.getRelativePath(on_timeout, off));
+        System.out.println(stateMachine.children);
 
-        
+        System.out.println("Downwards: " + stateMachine.findDownwardsPath(off_eetimeout));
+        System.out.println("Relative: " + stateMachine.getRelativePath(on_timeout, on));
 
-    }    
-    // other features ...
+        System.out.println(stateMachine.findDownwardsPath(stateMachine.getNodeByName("on_timeout")));
+
+    }
 }
